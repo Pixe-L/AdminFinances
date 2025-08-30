558bfa19578a3b1aa401a1f8ddc7ae33f647f77a
Arturo <arturcas2@hotmail.com>
2025-08-29 22:28:34 -0600

    Implementing css and app.vue

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<script setup>
+import Budget from './components/Budget.vue';
+
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Expense Manager</h1>
+
+      <div class="header-container container shade">
+        <Budget/>
+      </div>
+    </header>
+  </div>
+</template>
+
+<style>
+:root {
+  --blue: #3b82f6;
+  --white: #fff;
+  --light-gray: #f5f5f5;
+  --gray: #94a3b8;
+  --dark-gray: #64748b;
+  --dark: #000;
+}
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+body {
+  font-size: 1.6rem;
+  font-family: "Lato", sans-serif;
+  background-color: var(--light-gray);
+}
+h1 {
+  font-size: 4rem;
+}
+h2 {
+  font-size: 3rem;
+}
+header {
+  background-color: var(--blue);
+}
+header h1 {
+  padding: 3rem 0;
+  margin: 0;
+  color: var(--white);
+  text-align: center;
+}
+.container {
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+}
+.header-container {
+  margin-top: -5rem;
+  transform: translateY(5rem);
+  padding: 5rem;
+}
+.shade {
+  box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+  background-color: var(--white);
+  border-radius: 1.2rem;
+  padding: 5rem;
+}
+</style>
---
+++ src/components/Budget.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+</script>
+
+<template>
+    <form class="budget">
+        <div class="field">
+            <label for="budget-new">Define budget</label>
+            <input type="number" id="budget-new" class="budget-new" placeholder="Add your budget">
+        </div>
+
+        <input type="submit" value="Define budget">
+    </form>
+</template>
+
+
+<style scoped>
+.budget {
+    width: 100%;
+}
+.field {
+    display: grid;
+    gap: 2rem;
+}
+.budget input[type="number"] {
+    background-color: var(--light-gray);
+    border-radius: 1rem;
+    padding: 1rem;
+    border: none;
+    font-size: 2.2rem;
+    text-align: center;
+}
+.budget input[type="submit"] {
+    background-color: var(--blue);
+    border-radius: 0.5rem;
+    border: none;
+    padding: 1rem;
+    text-align: center;
+    font-size: 2rem;
+    font-weight: 900;
+    margin-block-start: 2rem;
+    color: var(--white);
+    width: 100%;
+    cursor: pointer;
+    transition: 0.5s;
+}
+.budget input[type="submit"]:hover {
+    background-color: #1048a4;
+}
+</style>
