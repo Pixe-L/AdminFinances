a8141e8884e3518c0037369de6ec9902879b93e4
Arturo <arturcas2@hotmail.com>
2025-08-29 23:37:40 -0600

    Adding script for budget

--- src/components/Budget.vue
+++ src/components/Budget.vue
@@ -1,12 +1,34 @@
 <script setup>
+    import {ref} from 'vue';
+    import Alert from './Alert.vue';
+
+
+    const budget = ref(0);
+    const error = ref('');
+
+    const defineBudget = () => {
+        if (budget.value <= 0) {
+            error.value = 'Presupuesto no valido'
+
+            setTimeout(() => {
+                error.value = '';
+            }, 3000);
+        } 
+    }
 
 </script>
 
 <template>
-    <form class="budget">
+    <form class="budget" @submit.prevent="defineBudget">
+
+        <alert v-if="error">
+            {{ error }}
+        </alert>
+
+
         <div class="field">
             <label for="budget-new">Define budget</label>
-            <input type="number" id="budget-new" class="budget-new" placeholder="Add your budget">
+            <input type="number" id="budget-new" class="budget-new" placeholder="Add your budget" v-model="budget">
         </div>
 
         <input type="submit" value="Define budget">
@@ -21,6 +43,11 @@
 .field {
     display: grid;
     gap: 2rem;
+}
+.budget label {
+    font-size: 2.2rem;
+    text-align: center;
+    color: var(--blue);
 }
 .budget input[type="number"] {
     background-color: var(--light-gray);
@@ -42,7 +69,7 @@
     color: var(--white);
     width: 100%;
     cursor: pointer;
-    transition: 0.5s;
+    transition: background-color 0.5s ease;
 }
 .budget input[type="submit"]:hover {
     background-color: #1048a4;
